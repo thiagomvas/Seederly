ea1f6b1593eba93c8f316a357e4505aea4422856
Thiago Menezes <thiagomvas@gmail.com>
2025-04-05 15:14:18 -0300

    chore: Add Avalonia project for desktop application

---
+++ Seederly.Desktop/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Seederly.Desktop.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ Seederly.Desktop/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Seederly.Desktop.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
