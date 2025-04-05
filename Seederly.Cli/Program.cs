91d8830c654697a046d576348b05a0e7cdae7a25
Thiago Menezes <thiagomvas@gmail.com>
2025-04-05 11:12:59 -0300

    feat: Add deep nested object support and add more generators

--- Seederly.Cli/Program.cs
+++ Seederly.Cli/Program.cs
@@ -4,14 +4,44 @@ using Seederly.Core;
 
 var mappings = new Dictionary<string, string>
 {
-    { "Date", "date.past"},
-    { "Info", "lorem.sentence"},
+    { "Id", "random.uuid" },
+    { "Timestamp", "date.past" },
+    { "Info", "lorem.paragraph" },
+
     { "User.FirstName", "name.firstName" },
     { "User.LastName", "name.lastName" },
     { "User.Email", "internet.email" },
     { "User.Age", "random.number" },
-    { "Adresses[2]", "{\"Street\": \"address.streetAddress\", \"City\": \"address.city\", \"State\": \"address.state\", \"ZipCode\": \"address.zipCode\", \"Country\": \"address.country\"}" },
+    { "User.Profile.Username", "internet.userName" },
+    { "User.Profile.AvatarUrl", "internet.url" },
+
+    { "User.Contact.Phone", "phone.number" },
+    { "User.Contact.Address.Street", "address.streetAddress" },
+    { "User.Contact.Address.City", "address.city" },
+    { "User.Contact.Address.ZipCode", "address.zipCode" },
+
+    // Array of objects
+    { "Addresses[3]", @"
+        {
+            ""Street"": ""address.streetAddress"",
+            ""City"": ""address.city"",
+            ""State"": ""address.state"",
+            ""ZipCode"": ""address.zipCode"",
+            ""Country"": ""address.country""
+        }"
+    },
+
+    // Nested array inside a user
+    { "User.PreviousOrders[2]", @"
+        {
+            ""OrderId"": ""random.uuid"",
+            ""Amount"": ""commerce.price"",
+            ""Date"": ""date.past"",
+            ""Product"": ""commerce.productName""
+        }"
+    },
 };
+
 
 var generator = new FakeRequestFactory();
 JsonObject fakeBody = generator.Generate(mappings);
--- Seederly.Core/FakeRequestFactory.cs
+++ Seederly.Core/FakeRequestFactory.cs
@@ -11,27 +11,83 @@ public class FakeRequestFactory
     private readonly Dictionary<string, Func<string>> _generators;
 
     public FakeRequestFactory()
+{
+    _generators = new()
     {
-        _generators = new()
-        {
-            ["name.firstName"] = () => _faker.Name.FirstName(),
-            ["name.lastName"] = () => _faker.Name.LastName(),
-            ["internet.email"] = () => _faker.Internet.Email(),
-            ["commerce.productName"] = () => _faker.Commerce.ProductName(),
-            ["commerce.price"] = () => _faker.Commerce.Price(),
-            ["random.uuid"] = () => Guid.NewGuid().ToString(),
-            ["random.number"] = () => Guid.NewGuid().ToString(),
-            ["address.streetAddress"] = () => _faker.Address.StreetAddress(),
-            ["address.city"] = () => _faker.Address.City(),
-            ["address.state"] = () => _faker.Address.State(),
-            ["address.zipCode"] = () => _faker.Address.ZipCode(),
-            ["address.country"] = () => _faker.Address.Country(),
-            ["date.past"] = () => _faker.Date.Past().ToString("o"),
-            ["date.future"] = () => _faker.Date.Future().ToString("o"),
-            ["lorem.sentence"] = () => _faker.Lorem.Sentence(),
-            ["lorem.paragraph"] = () => _faker.Lorem.Paragraph()
-        };
-    }
+        // Name
+        ["name.firstName"] = () => _faker.Name.FirstName(),
+        ["name.lastName"] = () => _faker.Name.LastName(),
+        ["name.fullName"] = () => _faker.Name.FullName(),
+        ["name.prefix"] = () => _faker.Name.Prefix(),
+        ["name.suffix"] = () => _faker.Name.Suffix(),
+
+        // Internet
+        ["internet.email"] = () => _faker.Internet.Email(),
+        ["internet.userName"] = () => _faker.Internet.UserName(),
+        ["internet.domainName"] = () => _faker.Internet.DomainName(),
+        ["internet.url"] = () => _faker.Internet.Url(),
+        ["internet.ip"] = () => _faker.Internet.Ip(),
+        ["internet.password"] = () => _faker.Internet.Password(),
+
+        // Address
+        ["address.streetAddress"] = () => _faker.Address.StreetAddress(),
+        ["address.city"] = () => _faker.Address.City(),
+        ["address.state"] = () => _faker.Address.State(),
+        ["address.zipCode"] = () => _faker.Address.ZipCode(),
+        ["address.country"] = () => _faker.Address.Country(),
+        ["address.latitude"] = () => _faker.Address.Latitude().ToString(),
+        ["address.longitude"] = () => _faker.Address.Longitude().ToString(),
+
+        // Commerce
+        ["commerce.productName"] = () => _faker.Commerce.ProductName(),
+        ["commerce.price"] = () => _faker.Commerce.Price().ToString(),
+        ["commerce.department"] = () => _faker.Commerce.Department(),
+        ["commerce.ean13"] = () => _faker.Commerce.Ean13(),
+
+        // Random
+        ["random.uuid"] = () => Guid.NewGuid().ToString(),
+        ["random.number"] = () => _faker.Random.Number().ToString(),
+        ["random.word"] = () => _faker.Random.Word(),
+        ["random.bool"] = () => _faker.Random.Bool().ToString(),
+        ["random.alphaNumeric"] = () => _faker.Random.AlphaNumeric(10),
+        ["random.hex"] = () => _faker.Random.Hexadecimal(),
+
+        // Date
+        ["date.past"] = () => _faker.Date.Past().ToString("o"),
+        ["date.future"] = () => _faker.Date.Future().ToString("o"),
+        ["date.recent"] = () => _faker.Date.Recent().ToString("o"),
+        ["date.soon"] = () => _faker.Date.Soon().ToString("o"),
+        ["date.birthday"] = () => _faker.Date.Past(30, DateTime.Now.AddYears(-18)).ToString("o"),
+
+        // Phone
+        ["phone.number"] = () => _faker.Phone.PhoneNumber(),
+
+        // Company
+        ["company.name"] = () => _faker.Company.CompanyName(),
+        ["company.catchPhrase"] = () => _faker.Company.CatchPhrase(),
+        ["company.bs"] = () => _faker.Company.Bs(),
+
+        // Finance
+        ["finance.account"] = () => _faker.Finance.Account(),
+        ["finance.amount"] = () => _faker.Finance.Amount().ToString("F2"),
+        ["finance.transactionType"] = () => _faker.Finance.TransactionType(),
+        ["finance.iban"] = () => _faker.Finance.Iban(),
+        ["finance.bic"] = () => _faker.Finance.Bic(),
+
+        // System
+        ["system.fileName"] = () => _faker.System.FileName(),
+        ["system.mimeType"] = () => _faker.System.MimeType(),
+        ["system.commonFileType"] = () => _faker.System.CommonFileType(),
+        ["system.directoryPath"] = () => _faker.System.DirectoryPath(),
+
+        // Lorem
+        ["lorem.word"] = () => _faker.Lorem.Word(),
+        ["lorem.words"] = () => string.Join(" ", _faker.Lorem.Words(3)),
+        ["lorem.sentence"] = () => _faker.Lorem.Sentence(),
+        ["lorem.paragraph"] = () => _faker.Lorem.Paragraph(),
+    };
+}
+
 
     public JsonObject Generate(Dictionary<string, string> map)
     {
@@ -41,7 +97,7 @@ public class FakeRequestFactory
         var nestedObjects = map
             .Where(kvp => kvp.Key.Contains('.'))
             .GroupBy(kvp => kvp.Key.Split('.')[0])
-            .ToDictionary(g => g.Key, g => g.ToDictionary(kvp => kvp.Key.Split('.')[1], kvp => kvp.Value));
+            .ToDictionary(g => g.Key, g => g.ToDictionary(kvp => string.Join("", kvp.Key.Split('.').Skip(1)), kvp => kvp.Value));
         // Add nested objects to the main JSON object
         foreach (var (key, value) in nestedObjects)
         {
@@ -49,7 +105,7 @@ public class FakeRequestFactory
         }
         
         var rangeMaps = map
-            .Where(kvp => kvp.Key.Contains('['))
+            .Where(kvp => !kvp.Key.Contains('.') && kvp.Key.Contains('['))
             .Select(kvp =>
             {
                 var match = Regex.Match(kvp.Key, @"(.+)\[(\d+)\]");
