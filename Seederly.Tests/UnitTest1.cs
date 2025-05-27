946836e2a173a01a413c755099940d037950b0bb
Thiago Menezes <thiagomvas@gmail.com>
2025-05-27 19:59:59 -0300

    chore: Add test project

---
+++ Seederly.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace Seederly.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
